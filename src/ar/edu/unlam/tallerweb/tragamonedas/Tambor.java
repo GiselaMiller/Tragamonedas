ee3273d838e14bdab927599fea8861586f256a50
Gisela <millergiselam@gmail.com>
2016-04-24 18:26:19 -0300

    Tragamonedas

--- src/ar/edu/unlam/tallerweb/tragamonedas/Tambor.java
+++ src/ar/edu/unlam/tallerweb/tragamonedas/Tambor.java
@@ -6,7 +6,12 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Tambor {
 	private Integer posicion;
 
-	public Integer girar() {
+	public void girar() {
+		ThreadLocalRandom.current().nextInt(1,8);
+	}
+	
+	public Integer obtenerPosicion(){
+		girar();
 		posicion=ThreadLocalRandom.current().nextInt(1,8);
 		return posicion;
 	}
--- test/ar/edu/unlam/tallerweb/tragamonedas/testTambor.java
+++ test/ar/edu/unlam/tallerweb/tragamonedas/testTambor.java
@@ -1,7 +1,5 @@
 package ar.edu.unlam.tallerweb.tragamonedas;
 
-
-import org.junit.Assert;
 import org.junit.Test;
 
 public class testTambor {
@@ -9,15 +7,15 @@ public class testTambor {
 	@Test
 	public void testPruebaDeGiro() {
 		Tambor miTambor= new Tambor();
-		Integer valorObtenido = miTambor.girar();
-		Assert.assertTrue(valorObtenido>=1 && valorObtenido<=8);
+		miTambor.girar();
 		
 	}
 	
 	@Test
 	public void testObtenerPosicion(){
 		Tambor miTambor= new Tambor();
-		miTambor.girar();
+		Integer posicion=miTambor.obtenerPosicion();
+		System.out.println(posicion); 
 	}
 
 }
